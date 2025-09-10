6fba1e3dea25c20d245d3b07b71748b9acd78e4f
gabriel <gabrielommello@gmail.com>
2025-09-10 20:07:20 -0300

    start

---
+++ PROJETO2/Avaliacao.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJETO2
+{
+    /// <summary>
+    /// Lógica interna para Avaliacao.xaml
+    /// </summary>
+    public partial class Avaliacao : Window
+    {
+        public Avaliacao()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_muitoposi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_posi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_nega_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PROJETO2/Pergunta1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJETO2
+{
+    /// <summary>
+    /// Lógica interna para Pergunta1.xaml
+    /// </summary>
+    public partial class Pergunta1 : Window
+    {
+        public Pergunta1()
+        {
+            InitializeComponent();
+        }
+
+        private void bnt_result_Click(object sender, RoutedEventArgs e)
+        {
+            RESULTADO resultado = new RESULTADO();
+            resultado.Show();
+            this.Close();
+        }
+    }
+}
